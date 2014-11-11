b207213fb78d1b7751638d0a2253019d46278ec8
Maria Padalko <mp@1212c0081.local>
2014-11-11 16:31:09 +0200

    From tabs to spaces.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 (function topMenuResponse(){
-	
-	var topMenu = $("#top-nav-menu"),
-	buttonResponseToggle = topMenu.find(".nav-response-toggle");
-	navResponseToggle = topMenu.find(">ul");
-		
-	buttonResponseToggle.on('click', function(){
-		navResponseToggle.slideToggle();
-	});
+  
+  var topMenu = $("#top-nav-menu"),
+  buttonResponseToggle = topMenu.find(".nav-response-toggle");
+  navResponseToggle = topMenu.find(">ul");
+    
+  buttonResponseToggle.on('click', function(){
+    navResponseToggle.slideToggle();
+  });
 })();
