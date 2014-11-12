55ecb8e96501725ffaaf9cde3a16e48d00b69590
Maria Padalko <mp@1212c0081.local>
2014-11-12 15:29:17 +0200

    Top slider fade slideshow, sidebar menu.

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,53 @@
 $(function() {
+  var slideShow = $('#slideshow'),
+      firstSlide = slideShow.find("li:first-child"),
+      lastSlide = slideShow.find("li:last-child"),
+      interval = 4000,
+      timer;
 
-  var itemSlider = $("#slideshow").find("li:eq(0)");
-      setTimeout(function(){
-          itemSlider.fadeOut();
-          itemSlider.next().fadeIn("li");
-      }, 4000 );
+  slideshowImgCenter();
+  slideshowTimeOut();
+  slideshow();
+
+  function slideshow(){
+    $('.arrow-wrapper').find('div').on('click', function(){
+       if ($(this).hasClass('left-arrow')){
+        turnSlide();
+       } else {
+        turnSlide(true);
+       }
+
+    })
+  }
+
+  function slideshowImgCenter(){
+    var imgs = $("#slideshow").find("img");
+    imgs.each(function(){
+      this.style.marginLeft = -(this.width/2) + 'px'
+    });
+  }
+
+  function slideshowTimeOut(){
+    setMyInterval();
+    slideShow.mouseenter(function() {
+      clearInterval(timer);
+    }).mouseleave(function(){
+      setMyInterval();
+    });
+  }
+
+  function setMyInterval() {
+    timer = setInterval(function(){turnSlide(true);}, interval);
+  }
+
+  function turnSlide(next) {
+    var activeSlide = $('#slideshow').find('li.active'),
+        nextSlide = next ? activeSlide.next("li") : activeSlide.prev("li"),
+        edgeSlide = next ? firstSlide : lastSlide;
+
+    activeSlide.removeClass("active");
+    if(nextSlide.length < 1) nextSlide = edgeSlide;
+    nextSlide.addClass("active");
+  }
 
 });
